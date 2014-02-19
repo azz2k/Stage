9be8bf6df90afdaea2d3b43aaf5e86f5a2f4d0dd
Christian Blum <blum@informatik.hu-berlin.de>
2014-02-19 17:38:20 +0000

    implemented a follow controller

---
+++ worlds/epuck/epuckFollow.cc
@@ -0,0 +1,44 @@
+#ifndef EPUCKFOLLOW_HH
+#define EPUCKFOLLOW_HH
+#include <stage.hh>
+
+using namespace Stg;
+
+#include "epuckBase.cc"
+
+class RobotFollow: public RobotBase
+{
+  protected:
+    virtual int myPositionUpdate(Model* model, RobotBase* robot)
+    {
+      this->Follow("epuck32", 0.3);
+      this->Avoidance();
+      
+      this->SetSpeed(LeftWheelVelocity,RightWheelVelocity);
+      return 0;
+    }
+
+  public:
+    RobotFollow(ModelPosition* pos):
+      RobotBase(pos)
+    {
+      std::cout << "RobotFollow constructor" << std::endl;
+    }
+    ~RobotFollow()
+    {
+      if (name)
+      {
+          free(name);
+          name = NULL;
+      }
+    }
+};
+
+// Stage calls this when the model starts up
+extern "C" int Init( Model* mod )
+{
+    RobotFollow *robot = new RobotFollow((ModelPosition*)mod);
+    return 0; //ok
+}
+
+#endif
